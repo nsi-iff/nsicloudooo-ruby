748355148057008e358d766132ae9060bd67939c
Douglas Camata <d.camata@gmail.com>
2012-03-01 03:04:39 -0300

    specs done

--- spec/nsicloudooo_spec.rb
+++ spec/nsicloudooo_spec.rb
@@ -1,11 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'base64'
+require 'nsisam'
 
-describe NSISam do
+$folder = File.expand_path(File.dirname(__FILE__))
+
+describe NSICloudooo do
 
   before :all do
-    @nsicloudooo = NSICloudooo::Client.new 'http://test:test@localhost:8888'
-    @keys = Array.new
+    @nsicloudooo = NSICloudooo::Client.new 'http://test:test@localhost:8886'
     @fake_cloudooo = NSICloudooo::FakeServerManager.new.start_server
   end
 
@@ -13,13 +15,56 @@ describe NSISam do
     @fake_cloudooo.stop_server
   end
 
-  context "granulation" do
-    it "can send a video to be granulated by a cloudooo node" do
-      document = Base64::encode64(File.new('26images-1table.odt', 'r').read)
-      response = @nsicloudooo.granulate(document)
+  context "simple granulation" do
+    it "can send a document to be granulated by a cloudooo node" do
+      response = @nsicloudooo.granulate(:file => 'document', :filename => 'test.odt')
       response.should_not be_nil
-      response.should have_key("images")
-      response.should have_key("tables")
+      response["key"].should == "key for document test.odt"
+    end
+  end
+
+  context "granulation with conversion" do
+    it "can send document in a closed format to be granulated by a cloudooo node" do
+      response = @nsicloudooo.granulate(:file => 'document', :filename => 'test.doc')
+      response.should_not be_nil
+      response["key"].should == "key for document test.doc"
+    end
+  end
+
+  context "granulation with download" do
+    it "can download documents from a link to be granulated by a cloudooo node" do
+      response = @nsicloudooo.granulate(:doc_link => "http://doc_link/test.doc")
+      response.should_not be_nil
+      response["key"].should == "key for document test.doc"
+    end
+  end
+
+  context "granualtion with callback" do
+    it "can send a document to be granulated by a cloudooo node and specify a callback url" do
+      response = @nsicloudooo.granulate(:file => 'document', :filename => 'test.odt', :callback => 'http://google.com')
+      response.should_not be_nil
+      response["key"].should == "key for document test.odt"
+      response["callback"].should == 'http://google.com'
+    end
+
+    it "can send a document to be granulated by a cloudooo node and specify the verb" do
+      response = @nsicloudooo.granulate(:file => 'document', :filename => 'test.odt', :callback => 'http://google.com', :verb => 'PUT')
+      response.should_not be_nil
+      response["key"].should == "key for document test.odt"
+      response["callback"].should == 'http://google.com'
+      response["verb"].should == 'PUT'
+    end
+  end
+
+  context "verify granulation" do
+    it "can verify is a granulation is done or no" do
+      key = @nsicloudooo.granulate(:file => 'document', :filename => '2secs.odt')["key"]
+      @nsicloudooo.done(key)["done"].should be_false
+      @nsicloudooo.done(key)["done"].should be_true
+    end
+
+    it "raises an error whentrying to verify if non-existing key is done" do
+      expect { @nsicloudooo.done("dont")["done"].should be_false }.to raise_error(NSICloudooo::Errors::Client::KeyNotFoundError)
     end
   end
 
